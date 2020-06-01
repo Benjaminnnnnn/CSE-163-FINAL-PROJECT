9ce712365ab1ac0a277a02113252ee44f9bb487b
Benjamin Zhuang <zhuanz3@uw.edu>
2020-06-01 12:03:19 -0700

    update testing

---
+++ py/testing.py
@@ -0,0 +1,36 @@
+'''
+Claire Luo, Samantha Shimogawa, Benjamin Zhuang
+CSE 163 Section AD
+
+This program tests the correctness of the world
+happiness analysis processes.
+'''
+from pandas.testing import assert_frame_equal
+
+
+def test_add_year():
+    pass
+
+
+def test_column_filter():
+    pass
+
+
+def test_rename_col():
+    pass
+
+
+def test_concatenate_dataframes():
+    pass
+
+
+def test_add_region():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
